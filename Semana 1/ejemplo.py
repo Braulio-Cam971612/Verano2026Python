a2ca6acf55cde6c83e888227cd40a83d705c561e
marioaje <marioaje@gmail.com>
2026-01-06 15:09:46 -0600

    Create ejemplo.py

---
+++ Semana 1/ejemplo.py
@@ -0,0 +1,27 @@
+def pidaDatos():
+    genero = input("Genero: ")
+    cedula = input("Ingrese su cedula")
+    residencia = input("Ingrese su residencia")
+    estadoCivil =  input("Ingrese su estado civil")
+    edad = int( input("Ingrese su edad") )
+
+    return genero, cedula, residencia, estadoCivil, edad
+
+def mostrarGenero(genero):
+        print ( "El genero es: ", genero)
+
+
+def esMayorEdad(edad):
+      if edad > 17:
+            print("Es mayor, de edad")
+      else:
+            print("Es menor, de edad")      
+
+
+#Programa inicial
+
+genero, cedula, residencia, estadoCivil, edad = pidaDatos()
+
+mostrarGenero(genero)
+
+esMayorEdad(edad)
