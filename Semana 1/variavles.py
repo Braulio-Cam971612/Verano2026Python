946451a474edb8e9f79f1930f1f433d2f1575d39
marioaje <marioaje@gmail.com>
2026-01-06 11:39:17 -0600

    Update variavles.py

--- Semana 1/variavles.py
+++ Semana 1/variavles.py
@@ -1,20 +1,35 @@
-# Variables
-nombre = "nombre"
-edad = 25
-precio = 19.23
-estado = False
+# # Variables
+# nombre = "nombre"
+# edad = 25
+# precio = 19.23
+# estado = False
 
-numeros  = [12, 34, 3,4,5]
+# numeros  = [12, 34, 3,4,5]
 
-print(precio)
-
-
-for items in numeros:
-    print(items)
-
-for i in range(10):
-    print( "I: ", i)
+# print(precio)
 
 
+# for items in numeros:
+#     print(items)
 
-    
+# for i in range(10):
+#     print( "I: ", i)
+
+
+# contador = 1
+
+# while contador < 20:
+#     print ("Valor", contador)
+#     contador += 1
+
+
+
+password = ""
+
+while password != "123":
+    print("Ingrese en el while")
+    password = input("Ingrese su password: ")
+
+
+if password == "123":
+    print("Sistema de incio")
