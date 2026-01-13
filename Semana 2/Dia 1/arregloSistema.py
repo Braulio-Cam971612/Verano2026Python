46510c16ec450110d71ed35316c3711b9ff6f93b
marioaje <marioaje@gmail.com>
2026-01-13 08:31:49 -0600

    Create arregloSistema.py

---
+++ Semana 2/Dia 1/arregloSistema.py
@@ -0,0 +1,16 @@
+#Mi variables
+personas = ["Mario", "Isaac", "Alberto","Sasha"]
+
+def modificar():
+    print("Modificando")
+
+print("Los usuarios del sistema", personas)
+modificar()
+usuarioNuevo = input("Ingrese un nuevo usuario")
+personas.append(usuarioNuevo)
+
+print("Los usuarios del sistema", personas)
+
+#La seccion de buscar
+#La seccion de modificar
+#La seccion de borrar
